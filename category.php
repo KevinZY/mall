46c80c2cbbc7a2c81c20efac13fa91f75727cf03
KevinKK <yzhang213@163.com>
2015-08-19 22:32:31 +0800

    finish category.php, show the goods

--- category.php
+++ category.php
@@ -15,4 +15,13 @@ $cat = new CatModel();
 $catList = $cat->getList();
 $catList = $cat->getListTree($catList, 0);
 
+$goods = new GoodsModel();
+if(isset($_GET['cat_id'])){
+    $goodsList = $goods->getGoodsByCat($_GET['cat_id']);
+    $catFamily = $cat->getFamilyTree($_GET['cat_id']);
+    $catFamily = array_reverse($catFamily);
+}else{
+    $goodsList = $goods->getGoodsByCat(0);
+}
+
 include(ROOT. 'view/front/lanmu.html');
--- include/config.inc.php
+++ include/config.inc.php
@@ -10,6 +10,6 @@ $_CFG = array();
 
 $_CFG['host'] = 'localhost';
 $_CFG['user'] = 'root';
-$_CFG['pwd'] = '';
+$_CFG['pwd'] = '1123';
 $_CFG['dbname'] = 'mall';
 $_CFG['charset'] = 'utf8';
